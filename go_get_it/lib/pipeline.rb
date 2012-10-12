13a33a81fc8b975bceaadac8feddd8f2417ecc42
Pedro Leal <pedrombl@gmail.com>
2012-10-12 16:57:22 -0300

    adding pipeline counter

--- go_get_it/lib/pipeline.rb
+++ go_get_it/lib/pipeline.rb
@@ -1,9 +1,10 @@
 class Pipeline
 
-  attr_accessor :name, :stages
+  attr_accessor :name, :stages, :executions
 
   def initialize(params)
     @name = params[:name]
     @stages = params[:stages] || []
+    @executions = params[:executions] || {}
   end
 end
--- go_get_it/spec/lib/pipeline_spec.rb
+++ go_get_it/spec/lib/pipeline_spec.rb
@@ -1,11 +1,14 @@
 require './lib/pipeline'
 require './lib/stage'
+require './lib/job_execution'
 
 describe 'Pipeline' do
   before :all do
     @pipeline = Pipeline.new(name: 'pipe')
     @stage = Stage.new(name: 'jobi')
     @pipeline.stages << @stage
+    @job_execution = JobExecution.new(id: 42)
+    @pipeline.executions[10] = @job_execution
   end
 
   subject { @pipeline }
@@ -13,4 +16,7 @@ describe 'Pipeline' do
   its(:name) { should eq('pipe') }
 
   its(:stages) { should include @stage } 
+
+  its(:executions) { should include(10 => @job_execution) }
+
 end
