b49a2428f344170452b7e3eba40d86632e107a95
Pedro Leal <pedrombl@gmail.com>
2012-10-15 14:04:10 -0300

    creating execution_timestamps from a csv row

--- go_get_it/lib/execution_timestamps.rb
+++ go_get_it/lib/execution_timestamps.rb
@@ -1,5 +1,6 @@
 class ExecutionTimestamps
-
+  SCHEDULED, ASSIGNED, PREPARING = [10, 11, 12] 
+  BUILDING, COMPLETING, COMPLETED = [13, 14, 15]
   attr_accessor :scheduled, :assigned, :preparing, :building, :completing, :completed
 
   def initialize(options)
@@ -9,5 +10,14 @@ class ExecutionTimestamps
     @building = options[:building]
     @completing = options[:completing]
     @completed = options[:completed]
+  end
+
+  def self.create_from_csv_row(csv_row)
+    ExecutionTimestamps.new(scheduled: csv_row[SCHEDULED],
+                            assigned: csv_row[ASSIGNED],
+                            preparing: csv_row[PREPARING],
+                            building: csv_row[BUILDING],
+                            completing: csv_row[COMPLETING],
+                            completed: csv_row[COMPLETED]) 
   end
 end
--- go_get_it/spec/lib/execution_timestamps_spec.rb
+++ go_get_it/spec/lib/execution_timestamps_spec.rb
@@ -18,5 +18,18 @@ describe 'ExecutionTimestamps' do
   its(:building) { should == "2012-04-19T12:43:02+04:00" }
   its(:completing) { should == "2012-04-19T12:43:35+04:00" }
   its(:completed) { should == "2012-04-19T12:43:35+04:00" }
+  
+  describe "create from csv row" do
+    csv_row = ["go_get_it", "beginning", "test",  "vm1.thoughtworks.com",  "50",  "123", "Passed",  "1", "1", "1", "2012-04-19T12:42:36+04:00", "2012-04-19T12:42:46+04:00", "2012-04-19T12:42:56+04:00", "2012-04-19T12:43:02+04:00", "2012-04-19T12:43:35+04:00", "2012-04-19T12:43:35+04:00", "0", "0", "27",  "3.766"]
 
+    it "creates a execution timestamps given a row from csv" do
+      execution_timestamps = ExecutionTimestamps.create_from_csv_row(csv_row) 
+      execution_timestamps.scheduled.should == "2012-04-19T12:42:36+04:00"
+      execution_timestamps.assigned.should == "2012-04-19T12:42:46+04:00"
+      execution_timestamps.preparing.should == "2012-04-19T12:42:56+04:00"
+      execution_timestamps.building.should == "2012-04-19T12:43:02+04:00"
+      execution_timestamps.completing.should == "2012-04-19T12:43:35+04:00"
+      execution_timestamps.completed.should == "2012-04-19T12:43:35+04:00"
+    end
+  end
 end
