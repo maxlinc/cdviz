853495f76dacd5e72d7b8db975c01b6f4e3896c9
Pedro Leal <pedrombl@gmail.com>
2012-10-15 14:15:12 -0300

    creating test report from a csv row

--- go_get_it/lib/tests_report.rb
+++ go_get_it/lib/tests_report.rb
@@ -1,9 +1,17 @@
 class TestsReport
+  FAILED_COUNT, IGNORED_COUNT, TOTAL_COUNT, DURATION = [16, 17, 18, 19]
   attr_accessor :total_count, :failed_count, :ignored_count, :duration
   def initialize(options)
     @total_count = options[:total_count]
     @failed_count = options[:failed_count]
     @ignored_count = options[:ignored_count]
     @duration = options[:duration]
+  end
+
+  def self.create_from_csv_row(csv_row)
+    new(total_count: csv_row[TOTAL_COUNT],
+        failed_count: csv_row[FAILED_COUNT],
+        ignored_count: csv_row[IGNORED_COUNT],
+        duration: csv_row[DURATION])
   end
 end
--- go_get_it/spec/lib/tests_report_spec.rb
+++ go_get_it/spec/lib/tests_report_spec.rb
@@ -13,4 +13,16 @@ describe 'TestsReport' do
   its(:failed_count) { should == 25 }
   its(:ignored_count) { should == 2 }
   its(:duration) { should == 1.25 }
+
+  describe "create from csv row" do
+    csv_row = ["go_get_it", "beginning", "test",  "vm1.thoughtworks.com",  "50",  "123", "Passed",  "1", "1", "1", "2012-04-19T12:42:36+04:00", "2012-04-19T12:42:46+04:00", "2012-04-19T12:42:56+04:00", "2012-04-19T12:43:02+04:00", "2012-04-19T12:43:35+04:00", "2012-04-19T12:43:35+04:00", "0", "1", "27",  "3.766"]
+
+    it "creates a tests report given a csv row" do
+      tests_report = TestsReport.create_from_csv_row(csv_row)
+      tests_report.total_count.should == "27"
+      tests_report.failed_count.should == "0"
+      tests_report.ignored_count.should == "1"
+      tests_report.duration.should == "3.766"
+    end
+  end
 end
