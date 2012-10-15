8af176dd7673be40da7c1b115b28eb8ef6b4ead5
Pedro Leal <pedrombl@gmail.com>
2012-10-15 15:21:09 -0300

    creating job_execution with tests_report and timestamps with csv row

--- go_get_it/lib/job_execution.rb
+++ go_get_it/lib/job_execution.rb
@@ -7,13 +7,17 @@ class JobExecution
     @agent = options[:agent]
     @duration = options[:duration]
     @result = options[:result]
+    @timestamps = options[:timestamps]
+    @tests_report = options[:tests_report]
   end
 
   def self.create_from_csv_row(csv_row)
-    JobExecution.new( agent: csv_row[AGENT],
-                      duration: csv_row[DURATION],
-                      id: csv_row[ID],
-                      result: csv_row[RESULT])
+    new( agent: csv_row[AGENT],
+         duration: csv_row[DURATION],
+         id: csv_row[ID],
+         result: csv_row[RESULT],
+         timestamps: ExecutionTimestamps.create_from_csv_row(csv_row),
+         tests_report: TestsReport.create_from_csv_row(csv_row))
   end
 
 end
--- go_get_it/spec/lib/job_execution_spec.rb
+++ go_get_it/spec/lib/job_execution_spec.rb
@@ -3,14 +3,14 @@ require './lib/execution_timestamps'
 require './lib/tests_report'
 describe 'JobExecution' do
   before :all do
+    @timestamps = ExecutionTimestamps.new({})
+    @tests_report = TestsReport.new({})
     @job_execution = JobExecution.new(id: 1234,
                                       agent: 'vm.agent.007',
                                       duration: 42,
-                                      result: 'Failed')
-    @timestamps = ExecutionTimestamps.new({})
-    @job_execution.timestamps = @timestamps
-    @tests_report = TestsReport.new({})
-    @job_execution.tests_report = @tests_report
+                                      result: 'Failed',
+                                      timestamps: @timestamps,
+                                      tests_report: @tests_report)
   end
 
   subject { @job_execution }
@@ -24,12 +24,27 @@ describe 'JobExecution' do
 
   describe "create from csv row" do
     csv_row = ["go_get_it", "beginning", "test",  "vm1.thoughtworks.com",  "50",  "123", "Passed",  "1", "1", "1", "2012-04-19T12:42:36+04:00", "2012-04-19T12:42:46+04:00", "2012-04-19T12:42:56+04:00", "2012-04-19T12:43:02+04:00", "2012-04-19T12:43:35+04:00", "2012-04-19T12:43:35+04:00", "0", "0", "27",  "3.766"]
-    it "create a job execution given a row from the csv" do
+
+    it "creates a job execution given a row from the csv" do
       job_execution = JobExecution.create_from_csv_row(csv_row)
       job_execution.agent.should == "vm1.thoughtworks.com"
       job_execution.duration.should == "50"
       job_execution.id.should == "123"
       job_execution.result.should == "Passed"
+    end
+
+    it "creates a job execution with timestamps" do
+      @timestamps = ExecutionTimestamps.new({})
+      ExecutionTimestamps.should_receive(:create_from_csv_row).with(csv_row) {@timestamps}
+      job_execution = JobExecution.create_from_csv_row(csv_row)
+      job_execution.timestamps.should == @timestamps
+    end
+
+    it "creates a job execution with tests report" do
+      @tests_report = TestsReport.new({})
+      TestsReport.should_receive(:create_from_csv_row).with(csv_row) {@tests_report}
+      job_execution = JobExecution.create_from_csv_row(csv_row)
+      job_execution.tests_report.should == @tests_report
     end
   end
 end
