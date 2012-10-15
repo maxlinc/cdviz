7620acd2dee0c3e82fba5c9f01755f5f69460324
Pedro Leal <pedrombl@gmail.com>
2012-10-15 14:03:22 -0300

    creating job_execution from a csv row

--- go_get_it/lib/job_execution.rb
+++ go_get_it/lib/job_execution.rb
@@ -1,4 +1,5 @@
 class JobExecution
+  AGENT, DURATION, ID, RESULT = [3, 4, 5, 6]
   attr_accessor :id, :agent, :duration, :result, :timestamps, :tests_report
 
   def initialize(options)
@@ -7,4 +8,12 @@ class JobExecution
     @duration = options[:duration]
     @result = options[:result]
   end
+
+  def self.create_from_csv_row(csv_row)
+    JobExecution.new( agent: csv_row[AGENT],
+                      duration: csv_row[DURATION],
+                      id: csv_row[ID],
+                      result: csv_row[RESULT])
+  end
+
 end
--- go_get_it/spec/lib/job_execution_spec.rb
+++ go_get_it/spec/lib/job_execution_spec.rb
@@ -21,4 +21,15 @@ describe 'JobExecution' do
   its(:result) { should == 'Failed' }
   its(:timestamps) { should == @timestamps }
   its(:tests_report) { should == @tests_report }
+
+  describe "create from csv row" do
+    csv_row = ["go_get_it", "beginning", "test",  "vm1.thoughtworks.com",  "50",  "123", "Passed",  "1", "1", "1", "2012-04-19T12:42:36+04:00", "2012-04-19T12:42:46+04:00", "2012-04-19T12:42:56+04:00", "2012-04-19T12:43:02+04:00", "2012-04-19T12:43:35+04:00", "2012-04-19T12:43:35+04:00", "0", "0", "27",  "3.766"]
+    it "create a job execution given a row from the csv" do
+      job_execution = JobExecution.create_from_csv_row(csv_row)
+      job_execution.agent.should == "vm1.thoughtworks.com"
+      job_execution.duration.should == "50"
+      job_execution.id.should == "123"
+      job_execution.result.should == "Passed"
+    end
+  end
 end
