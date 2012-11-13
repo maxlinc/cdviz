ccc39f042856c54f5ea3b5ab8fbfba78a22f0cbf
Pedro Leal <pedrombl@gmail.com>
2012-11-13 13:45:02 -0200

    reading csv file

--- src/main/groovy/thoughtworks/JobExecution.groovy
+++ src/main/groovy/thoughtworks/JobExecution.groovy
@@ -1,15 +1,22 @@
 package thoughtworks
+import static com.xlson.groovycsv.CsvParser.parseCsv
 
 class JobExecution {
-	private static final DURATION = 4; 
-	private csvRow;
+	def data
 	
-	public JobExecution(csvRow) {
-		this.csvRow = csvRow;
+	def propertyMissing(String name) { 
+		for (line in data) {
+			print data."$name"
+		}
 	}
 	
-	public getDuration() {
-		return csvRow[DURATION]
+	public JobExecution(csvFile) {
+		def reader = new FileReader(csvFile)
+		def file = new File(csvFile)
+		print file.getText()
+		data = parseCsv(file.getText(), autoDetect:true)
+		for (line in data) {
+			println line.cruise_job_duration
+		}
 	}
-
 }
--- src/test/groovy/thoughtworks/JobExecutionTest.groovy
+++ src/test/groovy/thoughtworks/JobExecutionTest.groovy
@@ -7,11 +7,7 @@ class JobExecutionTest {
 	
 	@Test
 	void shouldHaveDuration() {
-		def csvRow = ["pipeline", "stage", "job", "agent", "20", "job_id", 
-					  "job_result", "pl_counter", "pl_label", "stage_counter",
-					  "2012-04-19T12:42:36+04:00", "assigned", "preparing", "building",
-					  "completing", "2012-04-19T12:43:35+04:00"]
-		def jobExecution = new JobExecution(csvRow)
-		assertEquals("20", jobExecution.getDuration())  
+		def jobExecution = new JobExecution('fixtures/sample.csv')
+		assertEquals("32", jobExecution.cruise_job_duration)  
 	}
 }
